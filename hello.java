83bfbfb09443f38200ba840d2bbc3e6614f116de
santhi-94 <shanthi.bhogavalli@gmail.com0>
2018-11-30 23:45:24 +0530

    modify hello.java

--- hello.java
+++ hello.java
@@ -7,3 +7,16 @@ system.out.println (hello world);
 }
 
 }
+---------------------------
+---------------------------
+
+public class HelloWorld 
+
+{
+   public static void main(String[] args)
+
+ {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
