7036f5328137ba927329bc7abcfad9e74eace241
santhi-94 <shanthi.bhogavalli@gmail.com0>
2018-12-03 21:31:38 +0530

    hello.java

---
+++ hello.java
@@ -0,0 +1,30 @@
+hello world
+{
+public static void main ()
+
+{
+system.out.println (hello world);
+}
+
+}
+---------------------------
+---------------------------
+
+public class HelloWorld 
+
+{
+   public static void main(String[] args) #hello world
+
+ {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   
+}
+
+------------------------
+------------------------
+{
+hello world
+}
+=======
+}  # eof
