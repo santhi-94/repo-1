ca74e01524d7fc40707e6ad54e9c8d414b4da346
sbhogavalli <shanthi.bhogavalli@gmail.com>
2018-12-31 11:29:50 +0530

    Revert "hello.java"
    This reverts commit f480fd03c36906f3a1a761c0b0204faaec6968fa.

--- hello.java
+++ hello.java
@@ -13,7 +13,11 @@ system.out.println (hello world);
 public class HelloWorld 
 
 {
+<<<<<<< HEAD
    public static void main(String[] args) #hello world
+=======
+   public static void main(String[] args)
+>>>>>>> parent of f480fd0... hello.java
 
  {
       // Prints "Hello, World" in the terminal window.
